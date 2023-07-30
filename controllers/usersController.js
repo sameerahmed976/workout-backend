f2036f19d9f7756d9f8d5512890395c6ec0071ea
Mohammed Sameer Ahmed <sameerahmed976@gmail.com>
2023-07-30 12:00:31 +0530

    password fix

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -70,9 +70,7 @@ const validate = (user) => {
   const userSchema = Joi.object({
     username: Joi.string().min(3).max(30).required(),
     email: Joi.string().email().required(),
-    password: Joi.string()
-      .pattern(new RegExp("^[A-Za-z0-9]{3,30}$"))
-      .required(),
+    password: Joi.string().required(),
   });
 
   return userSchema.validate(user);
